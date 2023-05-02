e0d6cbe9f431be34087af89b6d8fd3e70ada1ef4
Gabantxo Vazquez <jgabantxo.ext@acciona.com>
2023-05-02 17:02:05 +0200

    Add folder for interval- and set-related operators and preliminar construction of files thereof

---
+++ sets_and_intervals/interval_operators.py
@@ -0,0 +1,9 @@
+#------------------#
+# Define functions #
+#------------------#
+
+def basic_interval_operator(interval_array,
+                            operation="union", 
+                            force_union=False):
+
+    # TODO: diseinatu pd.arrays.IntervalArray motako matrizea, "interval_array" zerrenda edo numpy matrize bat izanik
---
+++ sets_and_intervals/operators_sets.py
@@ -0,0 +1,23 @@
+#----------------#
+# Import modules #
+#----------------#
+
+import inspect
+
+#------------------#
+# Define functions #
+#------------------#
+
+def operations_with_sets(array_of_sets, operation="union"):
+
+    operation_opt_array = ["union", 
+                           "intersection", 
+                           "difference", 
+                           "symmetric_difference", 
+                           "cartesian_product"]
+
+    arg_names = get_obj_specs.__code__.co_varnames
+    operation_arg_pos = 
+    obj2ch_arg_pos = find_substring_index(arg_names, "obj2")
+    if operation not in operation_opt_array:
+        raise ValueError(f"Wrong '{operator_arg}' argument. Options are {operation_opt_array}.")
