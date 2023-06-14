7fd4c9829b8e74d46b369dd50ef101108851e8e1
Gabantxo Vazquez <jgabantxo_ext@acciona.com>
2023-06-14 16:54:52 +0200

    Fixed bugs in pytools repository path retriever. Default behaviour is now to delete the old path, but asking the user to delete hidden '.git' folder to delete manually

--- setup_repo.py
+++ setup_repo.py
@@ -5,23 +5,24 @@ Run this program if it is the first time using the repository
 or it has been re-cloned.
 """
 
-# =============================================================================
-# Import modules
-# =============================================================================
+#----------------#
+# Import modules #
+#----------------#
 
 import os
 from pathlib import Path
 import shutil
 
-# =============================================================================
-# Input parameters
-# =============================================================================
+#------------------#
+# Input parameters #
+#------------------#
 
 # Home path #
 home_path = Path.home()
 
 # Default path (same as the current one) 
 default_path = Path.cwd()
+default_path_str = str(default_path)
 
 # Ask the user if other than the default is preferred #
 custom_path\
@@ -43,17 +44,37 @@ custom_path_retriever_path = f"{home_path}/{custom_path_retriever_name}"
 lcp = len(custom_path)
 
 # If 'Enter' key is hit, then set the path to the default one #
+#-------------------------------------------------------------#
+
 if lcp == 0:
     custom_path = default_path
     
 # Otherwise, move the repo to the specified path #
-else:
-    shutil.copytree(default_path, custom_path, dirs_exist_ok=True)
+#------------------------------------------------#
+
+else:  
+    
+    # Define custom path with the directory as the current, default one #
+    default_dir = default_path.stem
+    customPathWithDefaultDir = f"{custom_path}/{default_dir}" 
+    
+    """
+    On Windows there is a problem when copying directories,
+    because some path can have double and/or quadruple backlashes and
+    forward slashes mixed, which is not practical to correct.
+    Instead, copy the entire directory and then delete the default one.
+    """
+    shutil.copytree(default_path, customPathWithDefaultDir, dirs_exist_ok=True)
+    
     print(f"Changed path from '{default_path}' to '{custom_path}'.")
     
     os.chdir(f"{default_path}/..")
-    shutil.rmtree(default_path, ignore_errors=True))    
-  
+    shutil.rmtree(default_path, ignore_errors=True)
+    
+    print(f"Deleted default path '{default_path}', but"
+          f"Git version controller directory '.git' will still be present.\n"
+          "Please delete the mentioned path manually.")
+    
 #------------------------------------------------------------------------------#
 # In either case, write a file containing a function that returns the path set #
 #------------------------------------------------------------------------------#
