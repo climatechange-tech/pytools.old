bd79d20abcfd46be6ce8971760d7b7c1af311d1d
climatechange-tech <jagabantxo@gmail.com>
2023-11-26 13:41:04 +0100

    String, tuple and dictionary argument insertion implemented on string formatter module at 'strings' directory.

--- strings/information_output_formatters.py
+++ strings/information_output_formatters.py
@@ -2,8 +2,21 @@
 # Define custom functions #
 #-------------------------#
 
-def print_fstring(format_string, arg_tuple):
-    print(format_string.format(*arg_tuple))
+def print_fstring(format_string, arg_obj):
+	if isinstance(arg_obj, str):
+        print(format_string.format(arg_obj))
+    elif isinstance(arg_obj, tuple):
+        print(format_string.format(*arg_obj))
+    elif isinstance(arg_obj, dict):
+        print(format_string.format(**arg_obj))
+	else:
+		raise TypeError("Argument must either be of type ´str´, ´tuple´ or ´dict´.")
         
-def print_percent_string(format_string, arg_tuple):
-    print(format_string %(arg_tuple))
+def print_percent_string(format_string, arg_obj):
+	if isinstance(arg_obj, str):
+	    print(format_string %(arg_obj))
+	elif isinstance(arg_obj, tuple):
+        print(format_string %(*arg_obj))
+    else:
+        raise TypeError("Argument must either be of type ´str´ or ´tuple´.")
+
