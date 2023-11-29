17fff904c55b6833b9104f2bb9efeb47b1fc3122
climatechange-tech <jagabantxo@gmail.com>
2023-11-29 23:36:11 +0100

    Bug fixed at the string formatter module, by forbidding to pass a tuple as an argument to the %-formatter string.

--- strings/information_output_formatters.py
+++ strings/information_output_formatters.py
@@ -3,20 +3,18 @@
 #-------------------------#
 
 def print_fstring(format_string, arg_obj):
-	if isinstance(arg_obj, str):
+    if isinstance(arg_obj, str):
         print(format_string.format(arg_obj))
     elif isinstance(arg_obj, tuple):
         print(format_string.format(*arg_obj))
     elif isinstance(arg_obj, dict):
         print(format_string.format(**arg_obj))
-	else:
-		raise TypeError("Argument must either be of type ´str´, ´tuple´ or ´dict´.")
-        
-def print_percent_string(format_string, arg_obj):
-	if isinstance(arg_obj, str):
-	    print(format_string %(arg_obj))
-	elif isinstance(arg_obj, tuple):
-        print(format_string %(*arg_obj))
     else:
-        raise TypeError("Argument must either be of type ´str´ or ´tuple´.")
-
+        raise TypeError("Argument must either be of type ´str´, ´tuple´ or ´dict´.")
+    
+def print_percent_string(format_string, arg_obj):
+    if isinstance(arg_obj, str):
+        print(format_string %(arg_obj))
+    else:
+        raise TypeError("Argument must be of type ´str´ only.")
+    
